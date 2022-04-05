2a772b4f1b2f0ce46db8f5b9d09c46ab65a12357
Lucas Souza <lhenriquesouza00@gmail.com>
2022-04-05 14:00:42 -0300

    Versão final do Projeto da Aula de React #01

---
+++ src/App.jsx
@@ -0,0 +1,47 @@
+import './App.css'
+import React from "react"
+
+import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
+import ComFilhos from './components/basicos/ComFilhos'
+import Card from './components/layout/Card'
+import Repeticao from './components/basicos/Repeticao'
+import Condicional from './components/basicos/Condicional'
+import CondicionalComIf from './components/basicos/CondicionalComIf'
+
+export default (props) => (
+    <div className="App">
+
+        <Card titulo="#06 - Condicional v2">
+            <CondicionalComIf numero={11} />
+        </Card>
+
+        <Card titulo="#05 - Condicional v1">
+            <Condicional numero={11} />
+        </Card>
+
+        <Card titulo="#04 - Repetição">
+            <Repeticao/>
+        </Card>
+
+        <Card titulo="#03 - Componente com Filhos">
+            <ComFilhos>
+                <ul>
+                    <li>Ana</li>
+                    <li>Bia</li>
+                    <li>Carlos</li>
+                    <li>Daniel</li>
+                </ul>
+            </ComFilhos>
+        </Card>
+
+        <Card titulo="#02 - Componente com Parâmetro">
+            <ComParametro titulo="Esse é o título" subtitulo="Esse é o subtítulo" />
+        </Card>
+
+        
+        <Card titulo="#01 - Primeiro Componente">
+            <Primeiro />
+        </Card>
+    </div>
+);
---
+++ src/components/layout/Card.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default props => 
+    <div className="Card">
+        <div className="Content">
+            {props.children}
+        </div>
+        <div className="Footer">
+            {props.titulo}
+        </div>
+    </div>
